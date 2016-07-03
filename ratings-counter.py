25be5078594786272548ea7b05d4e0273b19f2e9
Arpan Tolat <Arpan Tolat>
2016-07-02 20:10:50 -0500

    Added Comments

--- ratings-counter.py
+++ ratings-counter.py
@@ -1,13 +1,23 @@
+# This script will parse the ratings dataset and return a histogram of ratings, 1-5
+
 from pyspark import SparkConf, SparkContext
 import collections
 
+#Set master node- local, create spark context object.
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
+#Read data from the text file line by line and create and RDD with each line as a string in the list
 lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
+
+#Map values of lines RDD to ratings RDD by splitting each line and isolating index [2], movie ratings
 ratings = lines.map(lambda x: x.split()[2])
+
+#Count rating results
 result = ratings.countByValue()
 
+#Sort and print results 
 sortedResults = collections.OrderedDict(sorted(result.items()))
 for key, value in sortedResults.iteritems():
-    print "%s %i" % (key, value)
+    print "Rating: %s Number of movies:%i" % (key, value)
+ 
